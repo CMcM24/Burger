c8ed763039a2ddc1808fb5e821fcf9aed8c0be04
Colin McMahon <cmcmahon1012@gmail.com>
2019-09-04 14:02:47 -0400

    created the file structure for the app

---
+++ burger/db/schema.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS burgers_db;
+
+CREATE DATABASE burgers_db;
+
+USE DATABASE burgers_db;
+
+CREATE TABLE burgers (
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    burger_name VARCHAR(100) NOT NULL,
+    devoured BOOLEAN NOT NULL DEFAULT 0;
+);
+
+SELECT * FROM burgers;
---
+++ burger/db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name)
+VALUES ("The Original Burger"),
+       ("Bacon Double Cheesburger"),
+       ("Royale with Cheese");
